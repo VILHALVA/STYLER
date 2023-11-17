c04850b4786db78568b18a7788f2fa50ce93db10
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-11-17 11:42:57 -0500

    BETA 1.8.2

---
+++ BOAS MUSICAS/BUSCADOR/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchForm = document.getElementById('searchForm');
+    const searchButton = document.getElementById('searchButton');
+    const clearButton = document.getElementById('clearButton');
+    const errorMessage = document.getElementById('errorMessage');
+    const musicNameInput = document.getElementById('musicName');
+
+    const baseURL = "https://vilhalva.github.io/STYLER/BOAS%20MUSICAS/";
+
+    searchButton.addEventListener('click', function () {
+        const userInput = musicNameInput.value.trim().toUpperCase();
+
+        if (userInput === "") {
+            showError("Digite o nome da música");
+            return;
+        }
+
+        const formattedInput = userInput.replace(/\s+/g, '%20');
+        const musicURL = `${baseURL}${formattedInput}.html`;
+
+        checkMusicExistence(musicURL);
+    });
+
+    clearButton.addEventListener('click', function () {
+        musicNameInput.value = "";
+        errorMessage.textContent = "";
+    });
+
+    function checkMusicExistence(url) {
+        fetch(url, {
+            method: 'HEAD'
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = url;
+                } else {
+                    showError("Música não encontrada");
+                }
+            })
+            .catch(error => {
+                console.error('Erro na requisição HEAD:', error);
+                showError("Erro ao verificar a música");
+            });
+    }
+
+    function showError(message) {
+        errorMessage.textContent = message;
+    }
+});
