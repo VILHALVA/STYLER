7c540449446831e7136ed0cfde3f81a1a962222e
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-11-27 10:25:28 -0500

    BETA 2.0.3

--- 03_PAGINACAO/MENU_PRINCIPAL.js
+++ 03_PAGINACAO/MENU_PRINCIPAL.js
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', function () {
         'https://vilhalva.github.io/STYLER/02_CADASTRO%20E%20LOGIN/HTML/FAZER%20DOACAO.html'
     ];
 
-    if (!allowedPages.includes(previousPage)) {
+    const isJavaScriptDisabled = document.querySelector('noscript').offsetHeight > 0;
+
+    if (isJavaScriptDisabled) {
+        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ DESABILITOU O JAVASCRIPT. ISSO NÃO É PERMITIDO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+        window.location.href = 'https://vilhalva.github.io/STYLER/STYLER.html';
+    } 
+    else if (!allowedPages.includes(previousPage)) {
         alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE LOGIN OU DOAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
         window.location.href = 'https://vilhalva.github.io/STYLER/STYLER.html';
     }
