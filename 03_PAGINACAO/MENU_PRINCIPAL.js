60fa1c593e8be56b5c69d0134bcd17d33a53789e
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-11-27 20:56:14 -0500

    Update MENU_PRINCIPAL.js

--- 03_PAGINACAO/MENU_PRINCIPAL.js
+++ 03_PAGINACAO/MENU_PRINCIPAL.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE LOGIN OU DOAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
         window.location.href = 'https://vilhalva.github.io/STYLER/STYLER.html';
     }
-
+    
     document.body.classList.add('js-enabled');
 }); 
 
