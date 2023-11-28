6693c74f5f11c2d7686df3b271068c61b7bbcbb6
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-11-28 10:20:00 -0500

    Update SUBMENU.js

--- 01_MENU/JAVASCRIPT/SUBMENU.js
+++ 01_MENU/JAVASCRIPT/SUBMENU.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
     const previousPage = document.referrer;
     const allowedPages = [
         'https://vilhalva.github.io/STYLER/01_MENU/HTML/01_MENU%20PRINCIPAL.html'
     ];
 
     if (!allowedPages.includes(previousPage)) {
+        document.body.style.display = 'none';
         alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO MENU PRINCIPAL! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
         window.location.href = 'https://vilhalva.github.io/STYLER/STYLER.html';
     }
 
     document.body.classList.add('js-enabled');
-});
+};
