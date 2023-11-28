3a4ed9947f39fadcbd150d1d3a067590a1a1c97f
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-11-28 10:38:54 -0500

    Update script.js

--- BOAS MUSICAS/BUSCADOR/script.js
+++ BOAS MUSICAS/BUSCADOR/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const baseURL1 = "https://vilhalva.github.io/STYLER/BOAS%20MUSICAS/HTML/";
     const baseURL2 = "https://www.vagalume.com.br/search?q="
     const baseURL3 = "https://www.letras.mus.br/?q="
-    const baseURL4 = "https://www.youtube.com/results?search_query=musica+"
+    const baseURL4 = "https://www.youtube.com/results?search_query=musica:+"
 
     searchButton.addEventListener('click', function () {
         const userInput = musicNameInput.value.trim().toUpperCase();
