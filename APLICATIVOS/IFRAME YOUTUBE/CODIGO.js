2125cdd02fa93d57085e2f1ab412397ecbaf9559
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-01-26 13:52:28 -0500

    CORRIGIDO O "IFRAME DO YOUTUBE"

--- APLICATIVOS/IFRAME YOUTUBE/CODIGO.js
+++ APLICATIVOS/IFRAME YOUTUBE/CODIGO.js
@@ -1,106 +1,118 @@
 var audioContext;
-    var sourceNode;
-    var playButton = document.getElementById('playButton');
-    var stopButton = document.getElementById('stopButton');
-    var clearButton = document.getElementById('clearButton');
-    var voltarButton = document.getElementById('voltarButton');
-    var youtubeLinkInput = document.getElementById('youtubeLink');
+var sourceNode;
+var playButton = document.getElementById('playButton');
+var stopButton = document.getElementById('stopButton');
+var voltarButton = document.getElementById('voltarButton');
+var youtubeLinkInput = document.getElementById('youtubeLink');
 
-    function playAudio() {
-      var youtubeLink = youtubeLinkInput.value;
-      var videoId = extractVideoId(youtubeLink);
-      if (!videoId) {
-        console.error('URL do YouTube inválida');
-        return;
-      }
+function playAudio() {
+  var youtubeLink = youtubeLinkInput.value;
+  if (!youtubeLink.trim()) {
+    alert("POR FAVOR, INSIRA UM LINK DO YOUTUBE ANTES DE CLICAR EM PLAY!");
+    return;
+  }
 
-      var embedUrl = 'https://www.youtube.com/embed/' + videoId;
-      var iframe = document.createElement('iframe');
-      iframe.setAttribute('width', '560');
-      iframe.setAttribute('height', '315');
-      iframe.setAttribute('src', embedUrl);
-      iframe.setAttribute('frameborder', '0');
-      iframe.setAttribute('allowfullscreen', 'true');
+  var videoId = extractVideoId(youtubeLink);
+  if (!videoId) {
+    console.error('URL do YouTube inválida');
+    return;
+  }
 
-      var playerDiv = document.getElementById('player');
-      playerDiv.innerHTML = '';
-      playerDiv.appendChild(iframe);
+  var embedUrl = 'https://www.youtube.com/embed/' + videoId;
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('width', '560');
+  iframe.setAttribute('height', '315');
+  iframe.setAttribute('src', embedUrl);
+  iframe.setAttribute('frameborder', '0');
+  iframe.setAttribute('allowfullscreen', 'true');
 
-      if (!audioContext) {
-        audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        audioContext.resume();
-      }
+  var playerDiv = document.getElementById('player');
+  playerDiv.innerHTML = '';
+  playerDiv.appendChild(iframe);
 
-      var audioUrl = 'https://www.youtube.com/watch?v=' + videoId;
-      var audioElement = new Audio();
-      audioElement.src = audioUrl;
-      audioElement.crossOrigin = 'anonymous';
+  if (!audioContext) {
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    audioContext.resume();
+  }
 
-      var sourceNode = audioContext.createMediaElementSource(audioElement);
-      var splitter = audioContext.createChannelSplitter(2);
-      var merger = audioContext.createChannelMerger(2);
+  var audioUrl = 'https://www.youtube.com/watch?v=' + videoId;
+  var audioElement = new Audio();
+  audioElement.src = audioUrl;
+  audioElement.crossOrigin = 'anonymous';
 
-      sourceNode.connect(splitter);
-      splitter.connect(merger, 0, 0); 
-      splitter.connect(merger, 1, 1); 
+  sourceNode = audioContext.createMediaElementSource(audioElement);
+  var splitter = audioContext.createChannelSplitter(2);
+  var merger = audioContext.createChannelMerger(2);
 
-      merger.connect(audioContext.destination);
-      audioElement.play();
+  sourceNode.connect(splitter);
+  splitter.connect(merger, 0, 0); 
+  splitter.connect(merger, 1, 1); 
+
+  merger.connect(audioContext.destination);
+  audioElement.play();
+}
+
+function stopAudio() {
+  if (!youtubeLinkInput.value.trim()) {
+    alert("POR FAVOR, INSIRA UM LINK DO YOUTUBE ANTES DE CLICAR EM STOP!");
+    return;
+  }
+
+  var playerDiv = document.getElementById('player');
+  playerDiv.innerHTML = '';
+  
+  if (sourceNode) {
+    sourceNode.disconnect();
+    sourceNode = null;
+  }
+
+  if (audioContext) {
+    audioContext.close().then(function() {
+      audioContext = null;
+    });
+  }
+
+  clearInput();
+}
+
+function clearInput() {
+  youtubeLinkInput.value = '';
+  var playerDiv = document.getElementById('player');
+  playerDiv.innerHTML = '';
+  
+  if (sourceNode) {
+    sourceNode.disconnect();
+    sourceNode = null;
+  }
+
+  if (audioContext) {
+    audioContext.close().then(function () {
+      audioContext = null;
+    });
+  }
+}    
+
+function extractVideoId(url) {
+  var videoId = null;
+  var match;
+  match = url.match(/[?&]v=([^&#]+)/);
+  if (match) {
+    videoId = match[1];
+  }
+  else {
+    match = url.match(/youtu\.be\/([^&#]+)/);
+    if (match) {
+      videoId = match[1];
     }
+  }
+  
+  return videoId;
+}
 
-    function stopAudio() {
-      var playerDiv = document.getElementById('player');
-      playerDiv.innerHTML = '';
+function voltar() {
+  window.history.back();
+}
 
-      if (sourceNode) {
-        sourceNode.disconnect();
-        sourceNode = null;
-      }
-
-      if (audioContext) {
-        audioContext.close().then(function() {
-          audioContext = null;
-        });
-      }
-    }
-
-    function clearInput() {
-      youtubeLinkInput.value = '';
-      var playerDiv = document.getElementById('player');
-      playerDiv.innerHTML = '';
-      if (sourceNode) {
-        sourceNode.disconnect();
-        sourceNode = null;
-      }
-    
-      if (audioContext) {
-        audioContext.close().then(function () {
-          audioContext = null;
-        });
-      }
-    }    
-
-    function extractVideoId(url) {
-      var videoId = null;
-      var match;
-      match = url.match(/[?&]v=([^&#]+)/);
-      if (match) {
-        videoId = match[1];
-      }
-      else {
-        match = url.match(/youtu\.be\/([^&#]+)/);
-        if (match) {
-          videoId = match[1];
-        }
-      }
-      
-      return videoId;
-    }
-
-    function voltar() {
-      window.history.back();
-    }
-
-    playButton.addEventListener('click', playAudio);
-    stopButton.addEventListener('click', stopAudio);
-    clearButton.addEventListener('click', clearInput);
+playButton.addEventListener('click', playAudio);
+stopButton.addEventListener('click', stopAudio);
+voltarButton.addEventListener('click', voltar);
