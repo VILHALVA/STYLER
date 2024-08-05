cc45b99fc619f091ecc68c0c46bd05ba66f0aada
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-05 15:53:53 -0500

    VALIDAÇÃO 1.0

--- BOAS MUSICAS/BUSCADOR/VALIDACAO.js
+++ BOAS MUSICAS/BUSCADOR/VALIDACAO.js
@@ -7,7 +7,7 @@ window.onload = function () {
     if (!allowedPages.includes(previousPage)) {
         document.body.style.display = 'none';
         alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO SUBMENU! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
-        window.location.href = 'https://vilhalva.github.io/STYLER/STYLER.html';
+        window.location.href = '../../STYLER.html';
     }
 
     document.body.classList.add('js-enabled');
