33aa1c093e336179d80aeda5d49ed0e1b8f22b8a
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-05 18:53:42 -0500

    22

--- 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
+++ 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
@@ -2,12 +2,10 @@ window.onload = function () {
     const previousPage = document.referrer;
     const requiredSubstring = 'https://vilhalva.github.io/STYLER/02_ENTRAR_NO_SITE/HTML/ZZZ.html';
 
-    console.log('Referrer:', previousPage);
-
     // Permitir acesso se a página foi carregada a partir de um arquivo local
-    if (previousPage.startsWith('file://')) {
+    if (previousPage.startsWith('file://') || !previousPage) {
         document.body.classList.add('js-enabled');
-        console.log('Acesso permitido via file://');
+        console.log('Acesso permitido via file:// ou referrer não disponível');
         return;
     }
 
@@ -16,20 +14,15 @@ window.onload = function () {
         if (previousPage.includes(requiredSubstring)) {
             document.body.classList.add('js-enabled');
             console.log('Acesso permitido via HTTPS com URL esperada');
-            return;
         } else {
-            // Se a URL HTTPS não corresponde à esperada, exibe o alerta e redireciona
-            console.log('Acesso não permitido: URL HTTPS sem a substring esperada');
             document.body.style.display = 'none';
             alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE VALIDAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
             window.location.href = '../../STYLER.html';
-            return;
         }
+    } else {
+        // Para URLs que não são `file://` nem `https://`
+        document.body.style.display = 'none';
+        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA DE UMA LOCALIZAÇÃO NÃO PERMITIDA! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+        window.location.href = '../../STYLER.html';
     }
-
-    // Bloquear todos os outros acessos
-    console.log('Acesso não permitido: URL não é file:// nem HTTPS');
-    document.body.style.display = 'none';
-    alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA DE UMA LOCALIZAÇÃO NÃO PERMITIDA! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
-    window.location.href = '../../STYLER.html';
 };
