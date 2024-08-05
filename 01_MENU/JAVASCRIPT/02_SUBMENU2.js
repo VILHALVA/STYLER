58588f0b1a04023fe31cb6d9e373e1079978c19c
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-05 16:50:55 -0500

    Update 02_SUBMENU2.js

--- 01_MENU/JAVASCRIPT/02_SUBMENU2.js
+++ 01_MENU/JAVASCRIPT/02_SUBMENU2.js
@@ -2,12 +2,19 @@ window.onload = function () {
     const previousPage = document.referrer;
     const requiredSubstring = 'https://vilhalva.github.io/STYLER/01_MENU/HTML/01_MENU%20PRINCIPAL.html';
 
-    if (previousPage.startsWith('file://') || previousPage.includes(requiredSubstring)) {
+    // Permite acesso se o referenciador for uma URL local ou se for o referenciador correto
+    if (previousPage.startsWith('file://') || previousPage === requiredSubstring) {
         document.body.classList.add('js-enabled');
         return;
     }
 
-    document.body.style.display = 'none';
-    alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO MENU PRINCIPAL! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
-    window.location.href = '../../STYLER.html';
+    // Bloqueia acesso se for uma URL HTTPS e não contém a substring necessária
+    if (previousPage.startsWith('https://') && !previousPage.includes(requiredSubstring)) {
+        document.body.style.display = 'none';
+        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO MENU PRINCIPAL! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+        window.location.href = '../../STYLER.html';
+    } 
+    else {
+        document.body.classList.add('js-enabled');
+    }
 };
