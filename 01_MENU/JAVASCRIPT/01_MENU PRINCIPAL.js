84b68b32f0f3ed2944adec2575c63ebef1ec366a
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-05 19:11:12 -0500

    23

--- 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
+++ 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
@@ -2,24 +2,25 @@ window.onload = function () {
     const previousPage = document.referrer;
     const requiredSubstring = 'https://vilhalva.github.io/STYLER/02_ENTRAR_NO_SITE/HTML/ZZZ.html';
 
+    // Permitir acesso se a página foi carregada a partir de um arquivo local
     if (previousPage.startsWith('file://') || !previousPage) {
         document.body.classList.add('js-enabled');
         console.log('Acesso permitido via file:// ou referrer não disponível');
         return;
     }
 
+    // Verificar se a página anterior é uma URL HTTPS
     if (previousPage.startsWith('https://')) {
         if (previousPage.includes(requiredSubstring)) {
             document.body.classList.add('js-enabled');
             console.log('Acesso permitido via HTTPS com URL esperada');
-        } 
-        else {
+        } else {
             document.body.style.display = 'none';
             alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE VALIDAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
             window.location.href = '../../STYLER.html';
         }
-    } 
-    else {
+    } else {
+        // Para URLs que não são `file://` nem `https://`
         document.body.style.display = 'none';
         alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA DE UMA LOCALIZAÇÃO NÃO PERMITIDA! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
         window.location.href = '../../STYLER.html';
