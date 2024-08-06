6291e367592aaaa00cd305e96c2297dcfe40720c
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-05 19:06:04 -0500

    22

--- 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
+++ 01_MENU/JAVASCRIPT/01_MENU PRINCIPAL.js
@@ -2,14 +2,9 @@ window.onload = function () {
     const previousPage = document.referrer;
     const requiredSubstring = 'https://vilhalva.github.io/STYLER/02_ENTRAR_NO_SITE/HTML/ZZZ.html';
 
-    function VOLTAR() {
-        document.body.style.display = 'none';
-        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE VALIDAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
-        window.location.href = '../../STYLER.html';
-    }
-
     if (previousPage.startsWith('file://') || !previousPage) {
         document.body.classList.add('js-enabled');
+        console.log('Acesso permitido via file:// ou referrer não disponível');
         return;
     }
 
@@ -19,10 +14,14 @@ window.onload = function () {
             console.log('Acesso permitido via HTTPS com URL esperada');
         } 
         else {
-            VOLTAR();
+            document.body.style.display = 'none';
+            alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELA PÁGINA DE VALIDAÇÃO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+            window.location.href = '../../STYLER.html';
         }
     } 
     else {
-        VOLTAR();
+        document.body.style.display = 'none';
+        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA DE UMA LOCALIZAÇÃO NÃO PERMITIDA! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+        window.location.href = '../../STYLER.html';
     }
 };
