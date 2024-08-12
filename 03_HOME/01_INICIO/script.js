300ab9a26d71c589121381a34e8abb1b7af5def8
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-12 15:30:47 -0500

    BETA 2.5.78

--- 03_HOME/01_INICIO/script.js
+++ 03_HOME/01_INICIO/script.js
@@ -18,6 +18,29 @@ function mudouTamanho() {
     }
 }
 
+$(function () {
+    var totalImages = 10;
+    var carousel = $('#carousel');
+
+    for (var i = 1; i <= totalImages; i++) {
+        carousel.append('<img src="./VILHALVA/' + i + '.jpg" alt="VILHALVA ' + i + '">');
+    }
+
+    $('#carousel img:eq(0)').addClass("ativo").show();
+
+    setInterval(slideFunction, 3000);
+
+    function slideFunction() {
+        if ($('.ativo').next().length) {
+            $('.ativo').fadeOut().removeClass("ativo").next().fadeIn().addClass("ativo");
+        } 
+        else {
+            $('.ativo').fadeOut().removeClass("ativo");
+            $('#carousel img:eq(0)').fadeIn().addClass("ativo");
+        }
+    }
+});
+
 document.addEventListener('DOMContentLoaded', () => {
     const modeIcon = document.getElementById('mode_icon');
     const themeStylesheet = document.getElementById('theme-stylesheet');
