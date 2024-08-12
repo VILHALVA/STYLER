40f6fe9d227dc3f53324aebc2beff3e821483754
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-08-12 11:59:29 -0500

    BETA 2.5.78

--- 03_HOME/CONTATO/script.js
+++ 03_HOME/CONTATO/script.js
@@ -1,7 +1,22 @@
 function togglePopup() {
-    window.history.back();
+    const popup = document.getElementById("popup-1");
+    const closeButton = document.querySelector('.close-btn');
+
+    closeButton.classList.add('clicked');
+
+    setTimeout(() => {
+        popup.classList.remove("active");
+        setTimeout(() => {
+            window.history.back();
+        }, 300); 
+    }, 300);
 }
 
 window.onload = function() {
-    document.getElementById("popup-1").classList.remove("hidden");
+    const popup = document.getElementById("popup-1");
+    popup.style.display = "flex";
+
+    setTimeout(() => {
+        popup.classList.add("active");
+    }, 10);
 };
