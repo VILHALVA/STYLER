d364a3ad12c95a66f59503f14c4a13901b5292a6
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2024-10-17 14:28:43 -0500

    VERSÃO 2.7.6

---
+++ 04_SITE/MEUS SITES/PORTFOLIO INTERATIVO/js/script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    }
+});
