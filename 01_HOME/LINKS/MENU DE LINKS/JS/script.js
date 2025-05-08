9083fbad7229ba8ee7eb0a74e820da4911b4ac11
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2025-05-08 11:05:36 -0500

    VERSÃO 3.5.7
    * ✅ **Validação de `https` adicionada**: Como o anúncio é sempre exibido após o carregamento do site, agora a validação de `https` também foi implementada nas rotas `01_HOME` e `02_ENTRAR`.
    * ✅ **Refatoração dos Temas**: Anteriormente, os temas dos `MENUS` e `SUBMENUS` eram separados em dois arquivos: `TEMA_01.css` (Claro) e `TEMA_02.css` (Escuro). No entanto, muitos seletores globais se repetiam em ambos os arquivos. Com a atualização de hoje, os seletores foram reorganizados de forma mais eficiente:
      * 🔸`GERAL.css`: contém apenas os seletores globais (sem repetição).
      * 🔸`CLARO.css` e `ESCURO.css`: contêm exclusivamente os seletores relacionados às cores dos temas.
    * ✅ **Correções de bugs e pequenas melhorias.**

--- 01_HOME/LINKS/MENU DE LINKS/JS/script.js
+++ 01_HOME/LINKS/MENU DE LINKS/JS/script.js
@@ -2,18 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const modeIcon = document.getElementById('mode_icon');
     const themeStylesheet = document.getElementById('theme-stylesheet');
 
-    document.body.classList.add('js-enabled');
-
     modeIcon.addEventListener('click', () => {
         const isDark = themeStylesheet.getAttribute('href').includes('ESCURO.css');
 
         if (isDark) {
-            themeStylesheet.setAttribute('href', './CSS/CLARO.css');
+            themeStylesheet.setAttribute('href', '../CSS/CLARO.css');
             modeIcon.classList.remove('fa-sun');
             modeIcon.classList.add('fa-moon');
         } 
         else {
-            themeStylesheet.setAttribute('href', './CSS/ESCURO.css');
+            themeStylesheet.setAttribute('href', '../CSS/ESCURO.css');
             modeIcon.classList.remove('fa-moon');
             modeIcon.classList.add('fa-sun');
         }
