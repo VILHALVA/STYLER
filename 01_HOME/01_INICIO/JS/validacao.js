cdd2d1216d3c48d973048a94a9a296b077d8f2cd
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2025-05-10 11:27:43 -0500

    VERSÃO 3.6.2
    * ✅ **Refatoração das Media Queries 2.0**
      - Anteriormente, o site suportava apenas duas faixas de resolução: **telas grandes** (Computadores e TVs - `min-width: 768px`) e **telas pequenas** (Tablets e Celulares - `max-width: 768px`).
    
      - Agora, o sistema foi expandido para **quatro tamanhos distintos**, oferecendo **maior precisão no controle de fontes e cores**:
    
      1. **Telas Gigantes – Desktop {GG}**: `min-width: 1201px`
      2. **Telas Grandes – Laptop {G}**: `min-width: 993px` até `max-width: 1200px`
      3. **Telas Médias – Tablet {M}**: `min-width: 769px` até `max-width: 992px`
      4. **Telas Pequenas – Smartphone {P}**: `max-width: 768px`
    
      - Além disso, foi realizada uma **refatoração completa nos seletores CSS**, com nomenclaturas específicas para cada faixa de tela, como:
    `--cor-gg-1`, `--cor-m-1`, `--cor-p-1`, `--fonte-m-1`, entre outras.
    
      - **Importante**: Essa atualização está ativa apenas nas seguintes rotas:
    `01_HOME/01_INICIO`, `01_HOME/LINKS`, `02_ENTRAR/01_MENU PRINCIPAL`, `03_MENU/` e `MUSICAS`.

--- 01_HOME/01_INICIO/JS/validacao.js
+++ 01_HOME/01_INICIO/JS/validacao.js
@@ -1,35 +1,42 @@
 window.onload = function () {
     const previousPage = document.referrer;
-    const currentPageProtocol = window.location.protocol;
-    const requiredSubstring = 'https://vilhalva.github.io/STYLER/01_HOME/ANUNCIO/index.html';
+    const currentProtocol = window.location.protocol;
+    const requiredURL = 'https://vilhalva.github.io/STYLER/01_HOME/ANUNCIO/index.html';
+    const fallbackURL = 'https://vilhalva.github.io/STYLER/index.html';
 
-    function denyAccess() {
-        document.body.style.display = 'none';
-        alert('😡ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO ANUNCIO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
-        window.location.href = 'https://vilhalva.github.io/STYLER/index.html';
+    document.body.classList.add('escondido');
+
+    function permitirAcesso() {
+        document.body.classList.add('js-enabled');
+        document.body.classList.remove('escondido');
     }
 
-    if (currentPageProtocol === 'file:') {
-        document.body.classList.add('js-enabled');
+    function negarAcesso() {
+        alert('😡 ATENÇÃO: FOI DETECTADO QUE VOCÊ ACESSOU ESSA PÁGINA SEM PASSAR PELO ANÚNCIO! VOCÊ SERÁ REDIRECIONADO PARA A PÁGINA INICIAL!');
+        setTimeout(() => {
+            window.location.href = fallbackURL;
+        }, 100);
+    }
+
+    if (currentProtocol === 'file:') {
+        permitirAcesso();
         return;
     }
 
     if (!previousPage) {
-        denyAccess();
+        negarAcesso();
         return;
     }
 
     if (previousPage.startsWith('https://')) {
-        if (previousPage.includes(requiredSubstring)) {
-            document.body.classList.add('js-enabled');
-            document.body.style.display = 'block';
+        if (previousPage.includes(requiredURL)) {
+            permitirAcesso();
         } 
         else {
-            denyAccess();
+            negarAcesso();
         }
         return;
     }
 
-    denyAccess();
+    negarAcesso();
 };
-
